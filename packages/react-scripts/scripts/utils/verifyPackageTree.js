e0d0d308a6688b169973026ec42aeb9f0b69f526
Nico Sommi <nicosommi@gmail.com>
2018-03-17 18:45:22 -0300

    Merge master ts

--- packages/react-scripts/scripts/utils/verifyPackageTree.js
+++ packages/react-scripts/scripts/utils/verifyPackageTree.js
@@ -20,7 +20,6 @@ function verifyPackageTree() {
     // These are packages most likely to break in practice.
     // See https://github.com/facebook/create-react-app/issues/1795 for reasons why.
     // I have not included Babel here because plugins typically don't import Babel (so it's not affected).
-    'eslint',
     'jest',
     'webpack',
     'webpack-dev-server',
